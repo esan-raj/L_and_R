08a69f3a2876c0f32b1e063c3a896a21618c4c61
Esan Raj <ishanraj845@gmail.com>
2024-10-03 17:54:59 +0530

    Script function altered

--- L_and_R/LR/scraping.py
+++ L_and_R/LR/scraping.py
@@ -58,11 +58,11 @@ def initialize_driver():
     chrome_options.add_experimental_option("prefs", prefs)
     #
     # Add headless mode if desired
-    # chrome_options.add_argument("--headless")
-    # chrome_options.add_argument("--disable-gpu")
-    # chrome_options.add_argument("--window-size=1920x1080")
-    # chrome_options.add_argument("--no-sandbox")
-    # chrome_options.add_argument("--disable-dev-shm-usage")
+    chrome_options.add_argument("--headless")
+    chrome_options.add_argument("--disable-gpu")
+    chrome_options.add_argument("--window-size=1920x1080")
+    chrome_options.add_argument("--no-sandbox")
+    chrome_options.add_argument("--disable-dev-shm-usage")
 
     driver = webdriver.Chrome(service=service, options=chrome_options)
     driver.get("https://tmsreports.pmjay.gov.in/OneTMS/loginnew.htm")
